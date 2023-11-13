9444def1cab241f650b6c4ba20dac5a46d428d0b
varunv22 <varun.venna22@gmail.com>
2023-11-13 14:31:50 -0800

    Basic FrontEnd with name and login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Slide</h1>
-      </header>
+      <div className="slide-in">
+        <header className="App-header">
+          <h1></h1>
+        </header>
+        <div className="categories">
+          <div className="category-box">Slide</div>
+          <div className="category-box">Settings</div>
+          <div className="category-box">About</div>
+          <div className="category-box">Find Events</div>
+          <div className="category-box">My Events</div>
+          <div className="category-box">Login</div>
+        </div>
+        <div className="slogan">Where plans and people align</div>
+      </div>
     </div>
   );
 }
