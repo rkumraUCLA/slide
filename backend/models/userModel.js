48928b99313a9aa0c68c0a85d42ab010ac9c8820
isblender <isblender@ucla.edu>
2023-11-19 15:29:06 -0800

    update

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const validator = require('validator')
 
 const Schema = mongoose.Schema
 
@@ -25,5 +27,29 @@ const userSchema = new Schema({
         required: false
     }
 }, { timestamps: true })
+
+// static signup
+userSchema.statics.signup = async function(email, password) {
+    // data validation
+    if (!email || !password) {
+        throw Error('Email and password fields must be filled')
+    }
+    if (!validator.isEmail(email)) {
+        throw Error('Invalid email')
+    }
+    //if (!validator.isStrongPassword(password)) { throw Error ('Password weak')}
+    // email exists
+    const alreadyExists = await this.findOne({ email })
+    if (alreadyExists) {
+        throw Error('Email already exists')
+    }
+    // hashing password
+    const uniquify = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(password, uniquify)
+
+    const userData = await this.create({ email, password: hash })
+    
+    return userData
+}
 
 module.exports = mongoose.model('User', userSchema)
