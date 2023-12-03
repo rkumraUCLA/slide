fc05abd363b00469fae3f0c6da262fe38fc61c11
varunv22 <varun.venna22@gmail.com>
2023-12-03 01:12:31 -0800

    updated find events

--- src/Components/Find Events.js
+++ src/Components/Find Events.js
@@ -2,40 +2,43 @@ import React, { useState, useEffect } from 'react';
 import { ChakraProvider, Box, Text } from '@chakra-ui/react';
 
 function FindEvents() {
-  const [events, setEvents] = useState([]);
   const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch('/api/events')
       .then((response) => {
         console.log('Response:', response);
+
         if (!response.ok) {
           throw new Error(`HTTP error! Status: ${response.status}`);
         }
-        return response.json();
-      })
-      .then((data) => setEvents(data))
-      .catch((error) => setError(error.message));
 
+        const contentType = response.headers.get('Content-Type');
+
+        if (!contentType || !contentType.includes('text/html')) {
+          throw new Error('Invalid content type. Expected HTML.');
+        }
+
+        return response.text();
+      })
+      .then((htmlData) => {
+        console.log('HTML Content:', htmlData);
+        document.getElementById('yourHtmlContainerId').innerHTML = htmlData;
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+        setError(error.message);
+      });
   }, []);
 
   return (
     <ChakraProvider>
       <Box textAlign="center" paddingTop="20">
-        <Text fontSize="2xl">Find Events</Text>
+        <Text fontSize="2xl"></Text>
         {error ? (
           <p>Error fetching events: {error}</p>
         ) : (
-          <ul>
-            {events.map((event) => (
-              <li key={event._id}>
-                <h3>{event.title}</h3>
-                <p>Sport: {event.sport}</p>
-                <p>Spots Open: {event.spotsOpen}</p>
-                {/* Add more details as needed */}
-              </li>
-            ))}
-          </ul>
+          <div id="yourHtmlContainerId"></div>
         )}
       </Box>
     </ChakraProvider>
