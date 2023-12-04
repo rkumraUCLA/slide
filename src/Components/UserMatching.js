94194533b55d4d9f0b20826bd97586d939b858a9
deetpar123 <deetpar123@gmail.com>
2023-12-03 19:41:20 -0800

    created user matching page

---
+++ src/Components/UserMatching.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { ChakraProvider, Box, Text, Button } from '@chakra-ui/react';
+
+function UserMatching() {
+  return (
+    <ChakraProvider>
+      <Box textAlign="center" paddingTop="20">
+        <Text fontSize="2xl">My Events Page</Text>
+        <Link to="/create-event">
+          <Button variant="primary" style={{ background: 'blue', color: 'white', margin: '10px' }}>
+            Create Event
+          </Button>
+        </Link>
+      </Box>
+    </ChakraProvider>
+  );
+}
+
+export default UserMatching;
