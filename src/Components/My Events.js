4c64028b19255b5f02d5d627528e757821f960d7
varunv22 <varun.venna22@gmail.com>
2023-12-04 01:16:34 -0800

    npm start now starts both, added create event

--- src/Components/My Events.js
+++ src/Components/My Events.js
@@ -1,50 +1,21 @@
-// MyEvents.js
-
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { ChakraProvider, Box, Text, Button } from '@chakra-ui/react';
 
-const MyEvents = () => {
-  const [events, setEvents] = useState(null);
-
-  useEffect(() => {
-    const fetchEvents = async () => {
-      const response = await fetch('/api/events/');
-      const json = await response.json();
-
-      if (response.ok) {
-        setEvents(json);
-        console.log(json);
-      }
-    };
-
-    fetchEvents();
-  }, []);
-
+function MyEvents() {
   return (
     <ChakraProvider>
-      <Box className='home' mt={20} ml={4} mr={4}>
-        <Box className='events'>
-          {events &&
-            events.map((event) => (
-              <Box key={event._id} p={4} borderWidth="1px" borderRadius="md" mb={4}>
-                <Text fontSize="lg" fontWeight="bold">
-                  {event.title}
-                </Text>
-                <Text>
-                  Sport: {event.sport}, Open Spots: {event.spotsOpen}
-                </Text>
-                <Link to={`/event/${event._id}`}>
-                  <Button mt={2} colorScheme="teal" size="sm">
-                    View Details
-                  </Button>
-                </Link>
-              </Box>
-            ))}
-        </Box>
+      <Box textAlign="center" paddingTop="20">
+        <Text fontSize="2xl">My Events Page</Text>
+        <Link to="/create-event">
+          <Button variant="primary" style={{ background: 'blue', color: 'white', margin: '10px' }}>
+            Create Event
+          </Button>
+        </Link>
       </Box>
     </ChakraProvider>
   );
-};
+}
 
 export default MyEvents;
+
