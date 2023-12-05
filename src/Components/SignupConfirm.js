74fd5a9ac6c12120f3bf0fb4afcb9e373715f424
varunv22 <varun.venna22@gmail.com>
2023-12-04 18:07:34 -0800

    signupconfirm changes

--- src/Components/SignupConfirm.js
+++ src/Components/SignupConfirm.js
@@ -1,7 +1,15 @@
-
+import React from 'react';
+import { Box, Text } from '@chakra-ui/react';
 
 function SignupConfirm(){
-
+    console.log("HELLO");
+    return (
+        <Box p={4} borderWidth="1px" borderRadius="md" mt={20} ml={10} mr={10}>
+          <Text fontSize="xl" fontWeight="bold" color="green.500">
+            Congratulations! You have slid into this event!
+          </Text>
+        </Box>
+      );
 }
 
 export default SignupConfirm
