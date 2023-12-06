8fc57c6bf942cc2347a0dada28392380b30d4a2e
rkumraUCLA <rkumra@ucla.edu>
2023-12-05 18:12:32 -0800

    adding patch routes

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,7 +2,7 @@ const express = require('express')
 const route  = express.Router()
 
 // controller functions
-const { loginUser, signupUser, getUsers, getLeaderboard, getUserEvents } = require('../controllers/userController')
+const { loginUser, signupUser, getUsers, updateUser, addEvent, getLeaderboard } = require('../controllers/userController')
 
 // login route
 route.post('/login', loginUser)
@@ -10,10 +10,16 @@ route.post('/login', loginUser)
 // signup route
 route.post('/signup', signupUser)
 
-// get all the Events route
+// get all the users route
 route.get('/getUsers', getUsers)
 
 route.get('/getLeaderboard', getLeaderboard)
+
+// update user information
+route.patch('/updateUser', updateUser)
+
+// addevents
+route.patch('/:id/addEvent', addEvent)
 
 route.get('/getUserEvents/:id', getUserEvents)
 module.exports = route
