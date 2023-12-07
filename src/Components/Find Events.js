b36cab877dd7d5f12af42f4f44e8449fca1663cc
deetpar123 <deetpar123@gmail.com>
2023-12-06 21:08:27 -0800

    Updated find events page

--- src/Components/Find Events.js
+++ src/Components/Find Events.js
@@ -1,5 +1,3 @@
-// FindEvents.js
-
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { ChakraProvider, Box, Text, Button } from '@chakra-ui/react';
@@ -28,23 +26,45 @@ function FindEvents() {
 
   return (
     <ChakraProvider>
-      <Box bg="#f0f9ff" className='home' mt={20} ml={4} mr={4} style={{ zIndex: 1 }}>
-        <Box className='events' style={{ zIndex: 3 }}>
+      <Box bg="white" className='home' mt={20} ml={4} mr={4}>
+        <Box className='events' display="grid" gridTemplateColumns="repeat(3, 1fr)" gap={4}>
           {events &&
             events.map((event) => (
-              <Box key={event._id} p={4} borderWidth="1px" borderRadius="md" mb={4} style={{ zIndex: 3 }}>
-                <Text fontSize="lg" fontWeight="bold" mb={2}>
-                  {event.title}
-                </Text>
-                <Text>Sport: {event.sport}</Text>
-                <Text>Open Spots: {event.spotsOpen}</Text>
-                <Text>Date: {new Date(event.eventDate).toLocaleDateString('en-US')}</Text>
-                <Link to={`/event/${event._id}`}>
-                  <Button bgGradient="linear(to-r, #7dd3fc, #075985)" textColor="white" size="sm" mt={2}>
+              <Link key={event._id} to={`/event/${event._id}`} style={{ textDecoration: 'none' }}>
+                <Box
+                  p={4}
+                  borderWidth="1px"
+                  borderRadius="md"
+                  mb={4}
+                  bg="#f0f9ff"
+                  display="flex"
+                  flexDirection="column"
+                  justifyContent="center"
+                  alignItems="center"
+                  textAlign="center"
+                  transition="background-color 0.3s ease-in-out"
+                  _hover={{ bg: '#bee3f8' }}
+                >
+                  <Text fontSize="lg" fontWeight="bold" mb={2}>
+                    {event.title}
+                  </Text>
+                  <Text>
+                    Sport: {event.sport}
+                  </Text>
+                  <Text>
+                    Open Spots: {event.spotsOpen}
+                  </Text>
+                  <Button
+                    bgGradient="linear(to-r, #7dd3fc, #075985)"
+                    textColor="white"
+                    size="sm"
+                    mt={2}
+                    _hover={{ bgGradient: 'linear(to-r, #56a9e1, #05436d)' }}
+                  >
                     View Details
                   </Button>
-                </Link>
-              </Box>
+                </Box>
+              </Link>
             ))}
         </Box>
       </Box>
